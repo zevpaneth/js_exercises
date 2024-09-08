fcfb5f3c4d035574fc138983846fd18f5e239599
Zev Paneth <z0504167424@gmail.com>
2024-09-08 12:39:29 +0300

    initial

---
+++ 6ES_exercises.js
@@ -0,0 +1,17 @@
+function ex1(num, boolean){
+    const lines = "=".repeat(num);
+    if (boolean){
+        console.log(lines+">");
+    }else console.log("<"+lines);
+}
+
+function ex2(){
+    const arr = []
+    let num = prompt(`please enter a valid number`);
+    arr.push(num);
+    while (num != 0) {
+        let num = prompt(`please enter a valid number`);
+        /* עוד לא סיימתי */
+    }
+
+}
